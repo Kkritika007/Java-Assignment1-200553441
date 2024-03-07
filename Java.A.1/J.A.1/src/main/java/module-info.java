4c3c0143107f513014135376e11fe28ae24f0dbb
manik17778 <137009112+Kkritika007@users.noreply.github.com>
2024-03-06 23:43:08 -0500

    assignment1
    completed

--- Java.A.1/J.A.1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javaassign {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javaassign to javafx.fxml;
-    exports com.example.javaassign;
-}
